377fd89791dbd343bae296690c743ec1a63f4885
Arthur <arthur@Linux.myguest.virtualbox.org>
2023-11-15 17:12:25 +0400

    added fib function

--- fact.cpp
+++ fact.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 
 int fact (int);
+int fib (int);
 
 int main ()
 {
 	int num = 0; 
 	std::cin >> num;
 	std::cout << fact (num) << std::endl;
+	std::cout << fib (num) << std::endl;
+}
+int fib (int num)
+{
+	if (num <= 2){
+		
+		return num;
+	}
+
+	return fib (num - 1) + fib (num - 2);
 }
 
 int fact (int num)
